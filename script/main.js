51a278a9b72bebeae272375c8b3733d7e1a15ced
KreativeStore <101590938+KreativeStore@users.noreply.github.com>
2022-04-01 22:23:13 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,24 +1,3 @@
-// Run when the webpage is loaded
-window.addEventListener('load', () => {
-  Swal.fire({
-    title: 'Are you sure to play music in background?',
-    // text: "You won't be able to revert this!",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes',
-    cancelButtonText: 'No',
-  }).then((result) => {
-    if (result.isConfirmed) {
-      document.querySelector('.song').play();
-      resolveFetch().then(animationTimeline());
-    } else {
-      resolveFetch().then(animationTimeline());
-    }
-  })
-})
-
 // Import the data to customize and insert them into page
 const fetchData = () => {
   fetch("customize.json")
